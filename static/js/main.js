a83ea34cc3eef22197935f7bbd3285bdc01ddca6
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-27 13:11:50 +0300

    add functionality to order button

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,15 @@
 const closeBtn = document.querySelector('.fa-times-circle');
-const cartList = document.querySelector(".pop-cart")
-closeBtn.addEventListener('click', e => {
-  e.preventDefault();
-  cartList.classList.remove('flx');
+const cartList = document.querySelector(".pop-cart");
+const menuList = document.querySelectorAll('.odr');
+menuList.forEach(i => {
+  addEventListener('click', e => {
+    let item = e.target;
+    if (item.classList.contains("order")) {
+      cartList.classList.add("flx");
+    }
+  })
+  closeBtn.addEventListener('click', e => {
+
+    cartList.classList.remove('flx');
+  })
 })
