73594d541dbfd497e7b5a22d70e15f1c5f663565
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-27 14:05:51 +0300

    add class constructor to take in order details

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,19 @@
 const closeBtn = document.querySelector('.fa-times-circle');
 const cartList = document.querySelector(".pop-cart");
 const menuList = document.querySelectorAll('.odr');
+class Menu {
+  constructor(img, name, top, price) {
+    this.image = img;
+    this.name = name;
+    this.toppingPrice = top;
+    this.price = price;
+  }
+  get totalPrice() {
+    return this.toppingPrice + this.price
+  }
+}
 menuList.forEach(i => {
-  addEventListener('click', e => {
+  i.addEventListener('click', e => {
     let item = e.target;
     if (item.classList.contains("order")) {
       cartList.classList.add("flx");
