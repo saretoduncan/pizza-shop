229ab8d71c75a64526006ed52b7c0c15b66eb284
sareto <duncan.moiyo@student.moringaschool.com>
2021-06-28 15:27:46 +0300

    add functionality to pay tab

--- static/js/main.js
+++ static/js/main.js
@@ -1,20 +1,21 @@
-const closeBtn = document.querySelector('.fa-times-circle');
+const closeBtn = document.querySelectorAll('.fa-times-circle');
 const cartList = document.querySelector(".pop-op");
 const menuList = document.querySelectorAll(".odr");
 const price = document.querySelector('#price');
+const totalPayment = document.querySelector(".pymnt");
+const quantity = document.querySelector(".quantity")
 
 
 
 class Menu {
-  constructor(img, name, top, price) {
-    this.image = img;
-    this.name = name;
-    this.toppingPrice = top;
-    this.price = price;
-
+  constructor(sizePrice, toppingPrice, quantity, delivery) {
+    this.sizePrice = sizePrice;
+    this.toppingPrice = toppingPrice;
+    this.quantity = quantity;
+    this.delivery = delivery;
   }
   get totalPrice() {
-    return this.toppingPrice + this.price
+    return ((this.sizePrice + this.toppingPrice) * this.quantity) + this.delivery;
   }
 }
 
@@ -27,6 +28,7 @@ menuList.forEach(item => {
     let order = target.classList.contains("order");
     if (order) {
       cartList.classList.add("flx");
+
 
     }
 
@@ -47,9 +49,16 @@ document.querySelectorAll(".order").forEach(item => {
 })
 
 
-closeBtn.addEventListener('click', e => {
+closeBtn.forEach(item => {
 
-  cartList.classList.remove('flx');
+  item.addEventListener('click', e => {
+    let target = e.target
+    let parent = target.parentElement.parentElement;
+    if (parent.classList.contains("pop-op"))
+      cartList.classList.remove('flx');
+    else(parent.classList.contains("pop-ot"))
+    document.querySelector(".pop-ot").classList.remove("flx");
+  })
 })
 
 
@@ -76,7 +85,25 @@ let sizePrice = () => {
   }
   return sizePr;
 }
-console.log(sizePrice());
+
+
 document.querySelector(".addCat").addEventListener('click', e => {
   document.querySelector(".pop-ot").classList.add("flx");
+  let quantity = parseInt(document.querySelector(".quantity").value);
+
+  let toppingPrice = topPrice();
+  let size = sizePrice();
+  let delivery = 200;
+  let calcPay = new Menu(size, toppingPrice, quantity, delivery);
+  totalPayment.innerText = calcPay.totalPrice;
+  cartList.classList.remove('flx');
+
+})
+document.querySelector(".pay").addEventListener('click', () => {
+  let name = document.querySelector("#name").value;
+  let location = document.querySelector('#location').value;
+  let price = totalPayment.innerText;
+  alert(`Hi ${name} your order will be delivered to ${location} in the next 30 minutes`)
+  document.querySelector(".pop-ot").classList.remove("flx");
+
 })
